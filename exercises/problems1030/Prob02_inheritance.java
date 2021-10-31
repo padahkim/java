0d9c78c58c3e2047f41d0382e75a072e4cf23753
padahkim <padahkim@gmail.com>
2021-10-31 23:39:02 +0900

    solved problem 10/31

---
+++ exercises/problems1030/Prob02_inheritance.java
@@ -0,0 +1,72 @@
+package java1029_inheritance.prob;
+
+/*
+ * [주의]테스트를 할때 main( )메소드의 주석을 해제하세요.
+ * 
+  [main메소드와 Human클래스는 수정하지 마세요]
+ 1 Human 클래스를 상속 받은 StudentExam 클래스를 구현하시오.
+ 2 3개의 StudentExam 객체를 생성 하여 배열에 셋팅 한 후 각각의 
+   객체의 나이, 신장 출력 한다.
+
+ [출력결과]
+ name	나이	신장	 몸무게	학번
+ 홍길동	15	171	 81	   201101
+ 정길동	13	183	 72	   201102
+ 박길동	16	175	 65	   201103
+ */
+
+class Human {
+	String name;
+	int age;
+	int height;
+	int weight;
+
+	Human() {
+	}
+
+	Human(String name, int age, int height, int weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public String toString() {
+		String data = name + "\t" + age + "\t" + height + "\t" + weight;
+		return data;
+	}
+}//end Human
+
+class StudentExam extends Human {
+	String studentId;
+	
+	StudentExam(String name, int age, int height, int weight, String studentId){
+		super(name, age, height, weight);
+		this.studentId = studentId;
+	}
+	@Override
+	public String toString() {
+		return super.toString()+ "     " + studentId;
+	}
+}
+
+
+public class Prob02_inheritance {
+	public static void main(String args[]) {
+		StudentExam se[]=new StudentExam[3];
+		se[0]=new StudentExam("홍길동",15,171, 81, "201101");
+		se[1]=new StudentExam("정길동",13,183, 72, "201102");
+		se[2]=new StudentExam("박길동",16,175, 65, "201103");
+		
+		System.out.printf("%4s %5s %8s %8s %8s\n","name","나이","신장","몸무게","학번");
+		
+		for(StudentExam sm : se)
+		System.out.println(sm.toString());
+	}//end main()
+}//end class
+
+
+
+
+
+
---
+++ exercises/problems1030/Prob03_inheritance.java
@@ -0,0 +1,98 @@
+package java1029_inheritance.prob;
+
+/*
+ * 
+ * Movie클래스를 상속받아 MovieWork클래스를 구현하세요.
+ * MovieWork클래스에서 display( )메소드를 오버라이딩해서
+ * 아래와 같이 구현하세요.
+ * [출력결과]
+ * 영화제목:추격자
+* 감독 : 7, 배우 : 5
+* 영화총점 :12
+* 영화평점 : ☆☆☆☆
+* ***********************************
+* 영화제목:사도세자
+* 감독:9, 배우:10, 작품성:10, 대중성:8, 대본:9
+* 영화총점 : 46
+* 영화평점 : ☆☆☆☆☆
+ */
+
+class Movie {
+	String title; // 영화제목 - 멤버변수	
+	int director; // 감독점수 - 멤버변수
+	int actor; // 배우점수 - 멤버변수
+	
+	public Movie(String title,  int director, int actor) {		
+		this.title = title;		
+		this.director = director;
+		this.actor = actor;
+	}
+	
+	void display() {
+		
+		int total = director+ actor; 		
+		String result="";
+		System.out.printf("영화제목:%s\n",title);
+		System.out.printf("감독 : %d, 배우 : %d\n",director,actor);
+		System.out.printf("영화총점 :%d\n",total);
+		if (total >= 15)
+			result = "☆☆☆☆☆";
+		else if(total>=12)
+			result = "☆☆☆☆";
+		else if(total>=10)
+			result = "☆☆☆";
+		else
+			result = "☆☆";
+		System.out.println("영화평점 : " + result);
+	}
+	
+}
+
+class MovieWork extends Movie{
+	private int cinematic; //작품성
+	private int popular; //대중성
+	private int scenario; //시나리오
+	public MovieWork(String title,  int director, int actor, int cinematic, int popular, int scenario) {
+		super(title,director, actor);
+		this.cinematic = cinematic;
+		this.popular = popular;
+		this.scenario = scenario;
+	}
+	MovieWork(String title, int director, int actor){
+		super(title,director, actor);
+	}
+	
+	@Override
+	void display() {
+		int total = director+ actor+ cinematic+ popular+ scenario; 		
+		String result="";
+		System.out.printf("영화제목:%s\n",title);
+		System.out.printf("감독 : %d, 배우 : %d, 작품성: %d, 대중성: %d, 대본: %d\n",director,actor,cinematic,popular,scenario);
+		System.out.printf("영화총점 :%d\n",total);
+		if (total >= 15)
+			result = "☆☆☆☆☆";
+		else if(total>=12)
+			result = "☆☆☆☆";
+		else if(total>=10)
+			result = "☆☆☆";
+		else
+			result = "☆☆";
+		System.out.println("영화평점 : " + result);
+	
+	}
+}
+
+public class Prob03_inheritance {
+
+	public static void main(String[] args) {
+		Movie mv=new Movie("추격자",7,5);
+		mv.display();
+		
+		System.out.println("***********************************");
+		
+		// 매개변수 4개인 생성자로 객체 생성
+		MovieWork mk = new MovieWork("사도세자", 9, 10, 10,8,9);
+		mk.display();
+	}
+
+}
