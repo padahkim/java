b40276b99f7baa17e37086cb3749373f72ddcd30
padahkim <padahkim@gmail.com>
2021-11-26 14:05:16 +0900

    start mini app exercise

---
+++ mini-app/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        //memberServiceImpl.join(member);이건왜안됌? 인터페이스내의 메서드는 추상메서든데,,ㅇㅎ,,MemberServiceImpl의 인스턴스네 얘가,,,
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
